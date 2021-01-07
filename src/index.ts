b1c932694b8f9e4b419bea54e95cf61bbfaaf434
Frank Su <jhihwei.su@gmail.com>
2021-01-07 19:45:34 +0800

    add queryOrderList and optOrder to package

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,350 @@
-import getTime from 'date-fns/fp/getTime';
 import format from 'date-fns-tz/format';
-import { v4 as uuid } from 'uuid';
 import CryptoJS from 'crypto-js';
 import phin from 'phin';
+import querystring from 'query-string';
 
-export class Qunar {}
+import { Currency } from './currency';
+
+export type QunarConstructorInput = {
+	key: string;
+	baseUrl?: string;
+};
+
+export type QueryOrderListRequest = {
+	fromDate: Date;
+	toDate: Date;
+	version: string;
+};
+
+export enum OrderStatusCode {
+	WAITING_PAYMENT = 1,
+	WAITING_ROOM_CONFIRM = 2,
+	FAILED = 3,
+	WAITING_ROOM_ARRANGE = 4,
+	CANCELLED = 6,
+	ROOM_ARRANGE_SUCCESSFUL = 7,
+	ORDER_FINISHED = 12,
+	PAYMENT_CANCEL_PROCESSING = 13,
+	PAYMENT_CANCEL_SUCCESSFUL = 15,
+	PAID_AND_WAITING_ROOM_CONFIRM = 29,
+	PAID_AND_ROOM_CONFIRM_FAILED = 42,
+}
+
+export enum PayTypeCode {
+	PREPAY = 0,
+	PAYNOW,
+}
+
+export enum PromotionCode {
+	NONE = 'NONE',
+	REDPACKET = 'REDPACKET',
+}
+
+export enum VouchType {
+	HAS_VOUCH = 1,
+	NO_VOUCH,
+}
+
+export enum VouchCancel {
+	YES = 1,
+	NO,
+}
+
+export enum FaxStatus {
+	SEND_SUCCESS = 'SEND_SUCCESS',
+	SEND_FAIL = 'SEND_FAIL',
+	SENDING = 'SENDING',
+	BACK_SUCCESS = 'BACK_SUCCESS',
+	BACK_EXCEPTION = 'BACK_EXCEPTION',
+}
+
+export enum OrderBlackInfo {
+	NORMAL = 'normal',
+	REPORTED = 'reported',
+	BLACK = 'black',
+}
+
+export enum PromotionValid {
+	ACTIVE_AFTER_CHECKING_IN = 1,
+	ACTIVE_AFTER_CHECKING_OUT,
+	ACTIVE_WHEN_STAYING,
+}
+
+export enum OrderStatus {
+	NEW_ORDER = 'NEW_ORDER',
+	CONFIRMING = 'CONFIRMING',
+	CONFIRMED = 'CONFIRMED',
+	CANCELLED = 'CANCELLED',
+	REJECTED = 'REJECTED',
+	CHECKED_IN = 'CHECKED_IN',
+	CHECKED_OUT = 'CHECKED_OUT',
+	NO_SHOW = 'NO_SHOW',
+	DELETE = 'DELETE',
+}
+
+export enum PayStatus {
+	PAY = 'PAY',
+	PAY_SUCCESS = 'PAY_SUCCESS',
+	GUARANTEE_SUCCESS = 'GUARANTEE_SUCCESS',
+	UNFREEZE_SUCCESS = 'UNFREEZE_SUCCESS',
+	REFUND_SUCCESS = 'REFUND_SUCCESS',
+	GUARANTEE_REVOKE_SUCCESS = 'GUARANTEE_REVOKE_SUCCESS',
+	GUARANTEE_CONFIRM_SUCCESS = 'GUARANTEE_CONFIRM_SUCCESS',
+}
+
+export type Fax = {
+	faxAction: string;
+	faxContentUrl: string;
+	faxSuccessTime: string;
+	faxStatus: FaxStatus;
+};
+
+export type Promotion = {
+	validType: PromotionValid;
+	name: string;
+	description: string;
+};
+
+export type CallCenterTicket = {
+	action?: string;
+	note?: string;
+	time?: string;
+	createTime?: string;
+};
+
+export type QorderInfo = {
+	qhotelName?: string;
+	qroomName?: string;
+};
+
+export type QuserInfo = {
+	userId?: string;
+};
+
+export type Membership = {
+	cardNo?: string;
+	name?: string;
+	mobile?: string;
+	idCard?: string;
+	level?: string;
+};
+
+export type Order = {
+	orderNum: string;
+	statusCode: OrderStatusCode;
+	statusMsg: string;
+	payTypeCode: PayTypeCode;
+	payTypeMsg: string;
+	roomNum: number;
+	payMoney: number;
+	cityName: string;
+	hotelId: string;
+	roomId: string;
+	hotelName: string;
+	roomName: string;
+	orderDate: string;
+	checkInDate: string;
+	checkOutDate: string;
+	customerName: string;
+	contactName: string;
+	contactPhone: string;
+	contactEmail?: string;
+	request?: string;
+	remark?: string[];
+	customerIp?: string;
+	everyDayPrice: string;
+	originEveryDayPrice?: string;
+	promotionCode?: PromotionCode;
+	redPacketMoney?: number;
+	cashBackMoney?: number;
+	breakfast?: string;
+	isVouch?: VouchType;
+	vouchMoney?: number;
+	changeRule?: VouchCancel;
+	lastCancelTime?: string;
+	bedType?: string;
+	contactPhoneKey?: string;
+	channel?: string;
+	foreignCurrencyAmount?: number;
+	currencyType?: Currency;
+	faxList?: Fax[];
+	arriving?: boolean;
+	promotionList?: Promotion[];
+	phoneAttribution?: string;
+	lastUpdateTime?: number;
+	instantConfirm?: boolean;
+	orderRefundMoney?: number;
+	roomFee?: number;
+	refundRoomFee?: number;
+	totalBasePrice: number;
+	orderBlackInfo?: OrderBlackInfo;
+	keyTimes: {
+		payTime?: string;
+		refundTime?: string;
+		unfreezeTime?: string;
+	};
+	distributeChannel?: string;
+	distributeChannelOrderId?: string;
+	orderStatus?: OrderStatus;
+	payStatus?: PayStatus;
+	ccInfo?: CallCenterTicket[];
+	otaOrderId?: string;
+	qorderInfo?: QorderInfo;
+	quserInfo?: QuserInfo;
+	amended?: boolean;
+	priceFrom?: string;
+	extra?: { [key: string]: any };
+	membership?: Membership;
+};
+
+export type QueryOrderListResponse = {
+	ret: boolean;
+	errCode: string;
+	errMsg?: string;
+	data?: Order[];
+	totalSize: number;
+};
+
+export enum OrderOpt {
+	CONFIRM_ROOM_SUCCESS = 'CONFIRM_ROOM_SUCCESS',
+	CONFIRM_ROOM_FAILURE = 'CONFIRM_ROOM_FAILURE',
+	ARRANGE_ROOM = 'ARRANGE_ROOM',
+	ADD_REMARKS = 'ADD_REMARKS',
+	APPLY_UNSUBSCRIBE = 'APPLY_UNSUBSCRIBE',
+	AGREE_UNSUBSCRIBE = 'AGREE_UNSUBSCRIBE',
+	REFUSE_UNSUBSCRIBE = 'REFUSE_UNSUBSCRIBE',
+	CONFIRM_SHOW = 'CONFIRM_SHOW',
+	CONFIRM_NOSHOW = 'CONFIRM_NOSHOW',
+	AGREE_CASHBACK = 'AGREE_CASHBACK',
+	SEND_FAX = 'SEND_FAX',
+	SEND_SMS = 'SEND_SMS',
+	CC_REPLY = 'CC_REPLY',
+	ORDER_CHANGE = 'ORDER_CHANGE',
+}
+
+export enum ArrangeType {
+	NUMBER = 'NUMBER',
+	NAME = 'NAME',
+}
+
+export enum ChargeSettlement {
+	ONLINE = 'ONLINE',
+	DEBT = 'DEBT',
+	CASH = 'CASH',
+}
+
+export type OptOrderRequest = {
+	orderNum: string;
+	opt: OrderOpt;
+	arrangeType?: ArrangeType;
+	confirmationNumber?: string;
+	money?: number;
+	remark?: string;
+	faxSendPartnerName?: string;
+	faxStamp?: string;
+	faxReceiveName?: string;
+	faxReceiveFaxNumber?: string;
+	faxSender?: string;
+	faxSenderTelNumber?: string;
+	faxReceiver?: string;
+	faxReceiverTelNumber?: string;
+	faxSendType?: string;
+	prices?: string;
+	roomChargeSettlement?: ChargeSettlement;
+	otherChargeSettlement?: ChargeSettlement;
+	smsContent?: string;
+	note?: string;
+	otaOrderId?: string;
+};
+
+export type OptOrderResponse = {
+	ret: boolean;
+	statusCode?: OrderStatusCode;
+	statusDesc: string;
+	errorMsg: string[];
+	errMsg?: string;
+};
+
+type Request = {
+	url: string;
+	parameters: object;
+	signature: string;
+};
+
+export default class Qunar {
+	private key: string;
+	private baseUrl: string;
+
+	constructor(input: QunarConstructorInput) {
+		const { key, baseUrl = '' } = input;
+		this.key = key;
+		this.baseUrl = baseUrl;
+	}
+
+	private signature(propertyStr: string): string {
+		propertyStr = `${this.key}${propertyStr}`;
+
+		return CryptoJS.MD5(propertyStr).toString();
+	}
+
+	private async request(request: Request) {
+		const { url, parameters, signature } = request;
+		const param = {
+			...parameters,
+			hmac: this.signature(signature),
+		};
+
+		const urlForRequest = `${this.baseUrl}${url}?${querystring.stringify(
+			param
+		)}`;
+		const response = await phin(urlForRequest);
+		const { statusCode, body } = response;
+		if (!statusCode || statusCode < 200 || statusCode >= 400) {
+			throw new Error(body);
+		}
+
+		return JSON.parse(body);
+	}
+
+	async queryOrderList(
+		request: QueryOrderListRequest
+	): Promise<QueryOrderListResponse> {
+		const { fromDate, toDate, version } = request;
+		const parameters = {
+			fromDate: format(fromDate, 'yyyyMMddHHmmss'),
+			toDate: format(toDate, 'yyyyMMddHHmmss'),
+			version,
+		};
+		return await this.request({
+			url: '/otaQueryOrder',
+			parameters,
+			signature: `${parameters.fromDate}${parameters.toDate}${version}`,
+		});
+	}
+
+	async optOrder(request: OptOrderRequest): Promise<QueryOrderListResponse> {
+		const { orderNum, opt } = request;
+		const signature = [
+			'money',
+			'arrangeType',
+			'confirmationNumber',
+			'faxSendPartnerName',
+			'faxReceiveFaxNumber',
+			'faxSendType',
+			'prices',
+			'roomChargeSettlement',
+			'otherChargeSettlementOptOrderRequest',
+		].reduce((prev, key) => {
+			const requestForDataRetrieval: { [key: string]: any } = request;
+			const value: any = requestForDataRetrieval[key];
+
+			return `${prev}${(value || '').toString()}`;
+		}, `${orderNum}${opt}`);
+
+		return await this.request({
+			url: '/otaOpt',
+			parameters: request,
+			signature,
+		});
+	}
+}
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,3 +1,36 @@
+import Qunar, { OrderOpt } from '../src/index';
+import dotenv from 'dotenv';
+
+import startOfDay from 'date-fns/fp/startOfDay';
+import endOfDay from 'date-fns/fp/endOfDay';
+
+dotenv.config();
+
+const qunar = new Qunar({
+	key: 'YEZm2SMFKHqKx8Fv98oJU5RiYWXdNO1N',
+	baseUrl: 'http://k3sp.opentrade.qunar.com/api/ota',
+});
+
 describe('Qunar Integration Test', () => {
-	it('should pass the assertions', async () => {});
+	it('should pass the assertions', async () => {
+		const now = new Date();
+
+		const queryResponse = await qunar.queryOrderList({
+			fromDate: startOfDay(now),
+			toDate: endOfDay(now),
+			version: '3.7',
+		});
+
+		console.log(queryResponse);
+		expect(queryResponse).toBeDefined();
+
+		const optOrderResponse = await qunar.optOrder({
+			orderNum: '4544656465',
+			opt: OrderOpt.ADD_REMARKS,
+			remark: 'hello i am a remark',
+		});
+
+		console.log(optOrderResponse);
+		expect(optOrderResponse).toBeDefined();
+	});
 });
